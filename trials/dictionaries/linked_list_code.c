a154677d0ccabc2cb23a636d47cda374cb6c585e
Tinsuag <tinsaeareg.work@gmail.com>
2023-09-19 22:45:41 +0300

    done for today in linked list

--- trials/dictionaries/linked_list_code.c
+++ trials/dictionaries/linked_list_code.c
@@ -5,7 +5,28 @@ struct node{
     int data;
     struct node *link;
 };
+struct node* insert_data_index(struct node *head, int input_data, int index)
+{
+    struct node *temp, *ptr;
+    int n = 0;
 
+    ptr = head;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp -> data = input_data;
+    temp -> link = NULL;
+    index--;
+
+    while(index != 1)
+    {
+        ptr = ptr -> link;
+        index--;
+    }
+
+    temp -> link = ptr -> link;
+    ptr -> link = temp;
+
+    return head;
+}
 struct node* insert_data_beg(struct node *head, int input_data)
 {
     struct  node *temp;
@@ -78,10 +99,18 @@ int main(){
     
 
     //count_of_nodes(head);
-    //print_linked_list(head);
+    print_linked_list(head);
+    printf("-----------------------------------------------------\n");
 
     insert_data(head, 76);
+    print_linked_list(head);
+    printf("-----------------------------------------------------\n");
+
     head = insert_data_beg(head, 32);
+    print_linked_list(head);
+    printf("-----------------------------------------------------\n");
+
+    head = insert_data_index(head, 9812, 3);
 
     //count_of_nodes(head);
     print_linked_list(head);
