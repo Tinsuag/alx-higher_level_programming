9b3e549358c0c77cb1e92d8e57f91beb36e50aa1
Tinsuag <tinsaeareg.work@gmail.com>
2023-10-09 22:42:33 +0300

    removed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-lookup - returns the list of available attributes and methods of an object.
+lookup - returns the list of available attributes and
+         and methods of an object.
 """
+
 
 def lookup(obj):
     """Using the 'dir()' method"""
