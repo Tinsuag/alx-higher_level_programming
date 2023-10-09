0892ce25d471bc05fbad8329bd91b4f93539760f
Tinsuag <tinsaeareg.work@gmail.com>
2023-10-09 22:54:31 +0300

    test documents

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+print_sorted  - prints the list in assending order
+"""
+
+Class MyList(list):
+    """will sort the element"""
+
+    def print_sorted(self):
+        """print a list in sorted assending order"""
+        print(sprted(self))
+
