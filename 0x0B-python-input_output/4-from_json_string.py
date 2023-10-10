12f802f3cc741fdb5a972814a634b1fecf38cd05
Tinsuag <tinsaeareg.work@gmail.com>
2023-10-11 01:04:51 +0300

    question 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,4 +7,4 @@ Write a function that appends a string at the end of a text file
 def from_json_string(my_str):
     """turns the string into a json equvalent no"""
     import json
-    return json.dumps(my_obj)
+    return json.loads(my_str)
