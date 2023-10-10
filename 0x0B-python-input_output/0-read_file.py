c87677b7886e8db9ae4199febe7265896742c15b
Tinsuag <tinsaeareg.work@gmail.com>
2023-10-11 00:23:00 +0300

    added some comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+this program reads a file
+"""
+
 def read_file(filename=""):
+    """opening the file in secure way"""
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
