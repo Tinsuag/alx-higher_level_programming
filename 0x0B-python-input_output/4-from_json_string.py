4a01aedd4bf350bc2eba9a3db5c7f13eacc9fd4f
Tinsuag <tinsaeareg.work@gmail.com>
2023-10-11 01:01:09 +0300

    done

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write a function that appends a string at the end of a text file
+"""
+
+
+def from_json_string(my_str):
+    """turns the string into a json equvalent no"""
+    import json
+    return json.dumps(my_obj)
