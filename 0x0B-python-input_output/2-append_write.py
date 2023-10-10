aeab71113b9f2ae9486fb0dfb76db90728af3add
Tinsuag <tinsaeareg.work@gmail.com>
2023-10-11 00:47:14 +0300

    adjusted the mode of the write function

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ Write a function that appends a string at the end of a text file
 
 def append_write(filename="", text=""):
     """writes and returns the ampunt of characters entered"""
-    with open(filename, 2, mode='w', encoding='utf-8') as f:
+    with open(filename, mode='rb+', encoding='utf-8') as f:
         len = f.write(text)
 
     return len
