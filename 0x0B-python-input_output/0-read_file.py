f55d4152ab7d9fbf77e7cde515789467f7213265
Tinsuag <tinsaeareg.work@gmail.com>
2023-10-11 00:36:27 +0300

    done with the second one

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ this program reads a file
 
 def read_file(filename=""):
     """opening the file in secure way"""
-    with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        with open(filename, encoding="utf-8") as f:
+            for line in f:
+                print(line, end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this program writes a string into a second document
+"""
+
+
+def write_file(filename="", text=""):
+    """this will write"""
+    with open(filename, mode='w', encoding="utf-8") as f:
+        len = f.write(text)
+        
+    return len
